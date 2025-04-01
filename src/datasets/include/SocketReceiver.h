f3306fffce6a9533bc9a4cdd0867120bb160a577
Koji Abel <mcgoblinprogram@gmail.com>
2025-04-01 16:46:45 -0700

    added socketreceiver dataloader for socket communication

---
+++ src/datasets/include/SocketReceiver.h
@@ -0,0 +1,50 @@
+#ifndef SOCKETRECEIVER_H
+#define SOCKETRECEIVER_H
+
+#include "AllDataLoaders.h"
+
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#include <vector>
+#include <algorithm>
+
+class SocketReceiver : public DataLoader{
+public:
+    SocketReceiver();
+    ~SocketReceiver();
+    
+    // interface
+    void init() override;
+    void configure(const json &config) override;
+    void run() override;
+    void join() override;
+
+private:
+    void process();
+    bool getPacket(std::vector<uint8_t>& buffer) const;
+    void processPacket(std::vector<uint8_t>& buffer);
+
+    bool connectToServer(bool log);
+
+    const char* getIP(int arg_fd) const;
+    const char* getIP(struct sockaddr_storage& addr) const;
+
+    std::string name, server_ip, port;
+    int connections;
+
+    unsigned total_events, packet_counter, total_hits, max_retry_delay;
+    bool run_thread, is_connected, socket_created;
+    int fd;
+
+    uint8_t max_connection_retries, current_retry;
+    std::unique_ptr<EventData> curEvents;
+    Event thisEvent;
+};
+
+#endif
