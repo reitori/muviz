f95dfa17b760a16dedd1388854ec8edd4fc9b998
Koji Abel <mcgoblinprogram@gmail.com>
2025-06-18 11:32:34 -0700

    Global function getExecutableDir to get the directory of the executable. Necessary for loading file shaders.

--- src/util/include/util.hpp
+++ src/util/include/util.hpp
@@ -4,8 +4,21 @@
 #include <json.hpp>
 #include <vector>
 #include <string>
+#include <filesystem>
 
 // Mostly the defaults, but using 32 instead of 64 bit int/float
 using json=nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int32_t, std::uint32_t, float>;
+
+#if defined(_WIN32)
+    #include <windows.h>
+#elif defined(__APPLE__)
+    #include <mach-o/dyld.h>
+#elif defined(__linux__)
+    #include <unistd.h>
+#endif
+
+namespace viz{
+    std::filesystem::path getExecutableDir();
+}
 
 #endif
