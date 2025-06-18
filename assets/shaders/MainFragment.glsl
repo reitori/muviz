5ea0d3ef9477a5b910275c73881023e387b21f33
Koji Abel <mcgoblinprogram@gmail.com>
2025-06-18 11:17:59 -0700

    Removed unnecessary builds and comments. Added assets folder and CMake copies assets folder into executable directory.

---
+++ assets/shaders/MainFragment.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+
+in vec4 fragOut;
+
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = fragOut;
+}
---
+++ assets/shaders/MainVertex.glsl
@@ -0,0 +1,22 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aFrag;
+
+layout (location = 2) in vec4 aInstanceFrag;
+layout (location = 3) in mat4 aInstanceModel;
+
+uniform mat4 uModel;
+uniform mat4 uView;
+uniform mat4 uProj;
+uniform bool uIsInstanced;
+
+out vec4 fragOut;
+
+void main()
+{
+   mat4 modelTransform = uIsInstanced ? aInstanceModel : uModel;
+   gl_Position = uProj * uView * modelTransform * vec4(aPos, 1.0);
+   fragOut = uIsInstanced ? aInstanceFrag : aFrag;
+}
+
