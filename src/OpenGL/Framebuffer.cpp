eb0b54cf240c54dfa71c7b8ae12009144b3d42ec
Koji Abel <mcgoblinprogram@gmail.com>
2025-07-21 15:40:56 -0700

    Added multisampling flag for framebuffer for MSAA.
    Added getWidth and getHeight as well as bool flag to enable multisampling for Texture2D type for when used as a framebuffer attachment.

--- src/OpenGL/Framebuffer.cpp
+++ src/OpenGL/Framebuffer.cpp
@@ -6,10 +6,24 @@ namespace{
 }
 
 namespace viz{
-
-    Framebuffer::Framebuffer(int width, int height){
+    Framebuffer::Framebuffer(int width, int height, bool setMultisample, bool useRenderbufferAttachment){
         m_init = false;
+        m_multisample = setMultisample;
+        m_useRenderbuffer = useRenderbufferAttachment;
+
+        // Create Texture2D for framebuffer color attachment
+        m_targetTexture = std::make_shared<Texture2D>(width, height, "FramebufferTexture", false, m_multisample);
+        m_targetTexture->changeFormat(GL_RGB, GL_RGB, GL_UNSIGNED_BYTE);
+
         init(width, height);
+    }
+
+    Framebuffer::Framebuffer(std::shared_ptr<Texture2D>& texture, bool setMultisample = false, bool useRenderbufferAttachment = true){
+        m_init = false;
+        m_multisample = setMultisample;
+        m_useRenderbuffer = useRenderbufferAttachment;
+
+        m_targetTexture = texture;
     }
 
    void Framebuffer::init(int width, int height) {
@@ -21,17 +35,17 @@ namespace viz{
         glGenFramebuffers(1, &m_id);
         glBindFramebuffer(GL_FRAMEBUFFER, m_id);
 
-        // Create Texture2D for framebuffer color attachment
-        m_targetTexture = std::make_shared<Texture2D>(width, height, "FramebufferTexture", false);
-        m_targetTexture->changeFormat(GL_RGB, GL_RGB, GL_UNSIGNED_BYTE);
         m_targetTexture->bindTexture();
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_targetTexture->getID(), 0);
-
+        if(!m_multisample) glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_targetTexture->getID(), 0);
+        else { glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, m_targetTexture->getID(), 0); }
         // Depth/Stencil buffer
-        glGenRenderbuffers(1, &m_rbo);
-        glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
-        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+        if(m_useRenderbuffer){
+            glGenRenderbuffers(1, &m_rbo);
+            glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+            glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+            if(m_multisample) glRenderbufferStorageMultisample(GL_RENDERBUFFER, m_targetTexture->samples, GL_DEPTH24_STENCIL8, width, height);
+            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+        }
 
         GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
         if (status != GL_FRAMEBUFFER_COMPLETE) {
@@ -47,22 +61,34 @@ namespace viz{
         m_init = true;
     }
 
-    void Framebuffer::resize(int width, int height) {
-        GLint drawFBOid = 0, readFBOid = 0;
-        glGetIntegerv(GL_DRAW_FRAMEBUFFER_BINDING, &drawFBOid);
-        glGetIntegerv(GL_READ_FRAMEBUFFER_BINDING, &readFBOid);
+    void Framebuffer::changeTexture(std::shared_ptr<Texture2D>& texture){
+        m_targetTexture = texture;
+        resize(m_targetTexture->getWidth(), m_targetTexture->getHeight());
+    }
 
+    void Framebuffer::blitFramebuffer(const std::unique_ptr<Framebuffer>& toScreenFramebuffer){
+        if(m_multisample){
+            glBindFramebuffer(GL_READ_FRAMEBUFFER, m_id);
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, toScreenFramebuffer->m_id);
+            glBlitFramebuffer(0, 0, m_width, m_height, 0, 0, toScreenFramebuffer->m_width, toScreenFramebuffer->m_height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        }
+    }   
+
+    void Framebuffer::resize(int width, int height) {
         glBindFramebuffer(GL_FRAMEBUFFER, m_id);
             m_targetTexture->resize(width, height);
-            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_targetTexture->getID(), 0);
 
-            glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
-            glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-            glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+            if(!m_multisample) glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_targetTexture->getID(), 0);
+            else { glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, m_targetTexture->getID(), 0); }
 
+            if(m_useRenderbuffer){
+                glBindRenderbuffer(GL_RENDERBUFFER, m_rbo);
+                glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+                if(m_multisample) glRenderbufferStorageMultisample(GL_RENDERBUFFER, m_targetTexture->samples, GL_DEPTH24_STENCIL8, width, height);
+                glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_rbo);
+            }
+        
         glBindTexture(GL_TEXTURE_2D, 0);
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, readFBOid);
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, drawFBOid);
 
         m_width = width;
         m_height = height;
--- src/OpenGL/Framebuffer.h
+++ src/OpenGL/Framebuffer.h
@@ -7,14 +7,19 @@
 
 namespace viz
 {
+
     class Framebuffer{
         public:
             Framebuffer() = delete;
-            Framebuffer(int width, int height);
+            Framebuffer(int width, int height, bool setMultisample = false, bool useRenderbufferAttachment = true);
+            Framebuffer(std::shared_ptr<Texture2D>& texture, bool setMultisample = false, bool useRenderbufferAttachment = true);
 
             void init(int width, int height);
             inline void bind() {glViewport(0, 0, m_width, m_height); glBindFramebuffer(GL_FRAMEBUFFER, m_id);}
             inline void unbind()  {glBindFramebuffer(GL_FRAMEBUFFER, 0);}
+
+            void blitFramebuffer(const std::unique_ptr<Framebuffer>& toScreenFramebuffer);
+            void changeTexture(std::shared_ptr<Texture2D>& texture);
 
             inline std::uint16_t getWidth() {return m_width;}
             inline std::uint16_t getHeight() {return m_height;}
@@ -28,7 +33,7 @@ namespace viz
             std::shared_ptr<Texture2D> m_targetTexture;
             GLuint m_id, m_rbo;
             std::uint16_t m_width, m_height;
-            bool m_init;
+            bool m_init, m_multisample, m_useRenderbuffer;
     };
 }
 
